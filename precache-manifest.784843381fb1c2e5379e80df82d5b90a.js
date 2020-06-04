791f46253f92d9379f7c3c38b62a3ccce3f63b67
kinodelnya <kinodelnya@users.noreply.github.com>
2020-06-04 22:18:45 +0400

    Updates

---
+++ precache-manifest.784843381fb1c2e5379e80df82d5b90a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b0dd46694fb13dc9cb8b0701731f96a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "539c9d59a02c313bb757",
+    "url": "/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "3f7c0be29cd96c1ab537",
+    "url": "/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "539c9d59a02c313bb757",
+    "url": "/static/js/2.c35796bf.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/static/js/2.c35796bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f7c0be29cd96c1ab537",
+    "url": "/static/js/main.0ba2c525.chunk.js"
+  },
+  {
+    "revision": "a64eb6240223ca74fc6c",
+    "url": "/static/js/runtime-main.679f7a16.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/static/media/persik.4e1ec840.png"
+  }
+]);
